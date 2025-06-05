055ce45ab486325be59041fd603e390caffef4b5
Franco <fjcopati@gmail.com>
2025-06-05 16:56:08 -0300

    welcome page styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,19 +5,20 @@ import Footer from "./components/Footer"
 // import NavBar from "./components/NavBar"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Toaster richColors />
-      <div className='flex justify-center items-center w-full h-full pb-10'>
-        <div className='container max-w-[600px] '>
-          <Routes>
-            <Route path="/" element={<WelcomePage />} />
-          </Routes>
-        </div>
-      </div>
-      <Footer />
-    </BrowserRouter>
-  )
+
+    return (
+        <BrowserRouter>
+            <Toaster richColors />
+            <div className=''>
+                <div className='container max-w-[100%] '>
+                    <Routes>
+                        <Route path="/" element={<WelcomePage />} />
+                    </Routes>
+                </div>
+            </div>
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
 export default App
--- client/src/pages/WelcomePage.tsx
+++ client/src/pages/WelcomePage.tsx
@@ -1,19 +1,42 @@
+import { useState } from "react";
+import "./test.css"
+
 
 const WelcomePage = () => {
-    return (
-        <div className='flex'>
+    const [username, setUsername] = useState("");
 
-            <div className="h-[calc(100vh-140px)] flex-1 flex flex-col justify-center items-center bg-gray-100 mt-4 ">
-                <div className="w-full max-w-md p-8 bg-white rounded-xl shadow-lg">
-                    <h1 className="text-4xl font-semibold text-gray-800 text-center">
-                        Welcome
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(username); // Datos del formulario
+    };
+
+    return (
+        <div className="relative h-screen w-screen flex items-center justify-center">
+            {/* Capa de fondo con degradado y máscara */}
+            <div className="absolute inset-0 gradient-mask bg-gradient-to-b from-[#15171f] via-[#14161C] to-[#0F1115] z-0"></div>
+
+            <div className="z-10 flex-1 flex justify-center items-center mt-4 ">
+                <div className="w-full max-w-md p-8 md:-translate-x-24  flex flex-col items-start gap-4">
+                    <h1 className="text-5xl font-semibold text-gray-100 text-center">
+                        Welcome 👋
                     </h1>
-                    <h2 className='text-lg text-gray-600 my-4 text-center'>
-                        Login with a username
+                    <h2 className='text-lg text-gray-300 my-4 text-center'>
+                        Set a username to get started
                     </h2>
+                    <form className="flex flex-col gap-2 w-full" method="post" >
+                        <div className="flex flex-col items-start justify-between gap-1 rounded-lg shadow-lg bg-slate-700 p-4">
+
+                            <label className="-mt-2 text-gray-400 w-full" htmlFor="username">Username</label>
+                            <input id="username" name="username" onChange={handleInputChange} className="text-gray-100 text-lg focus:outline-none autofill:shadow-inner autofill:bg-slate-600" type="text" />
+                        </div>
+                        <button onClick={handleSubmit} className="p-3 rounded-lg bg-amber-600 text-gray-100 hover:cursor-pointer hover:bg-amber-700" >Send</button>
+                    </form>
                 </div>
             </div>
-
         </div>
     )
 }
